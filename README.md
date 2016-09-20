0b95063eb5f58fb60940d6e0bc102cdcb7b94d80
Stefan Schöberl <stefan.schoeberl@icloud.com>
2016-09-21 00:14:56 +0200

    Implemented basic functions: - authentication via http - experimental upload of images - app.js takes domain as parameter
    Restructured parts of the code:
    - moved authentication out of app.js
    
    Updated:
    - .gitignore
    - Readme

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # ACI-Server
 A minimalistic node server that provides .aci files for rkt discovery.
 
-Note that this programm is under heavy development and not ready for deployment yet!
+Note that this program is under heavy development and not ready for deployment yet!
 
 ## How it works
 This server runs behind an existing webserver and provides .aci images.
 It provides the links necessary for ac-discovery.
+
+## Start server
+```bash
+$ node app.js you.domain.com
+```
+
--- app.js
+++ app.js
@@ -1,18 +1,73 @@
+const domain = process.argv[2];
+
+// General libraries
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+// Express
 var express = require('express');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+
+// Authentication library
+var auth = require('./lib/auth');
+
+// Basic Express setup
 var app = express();
 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
+app.set('view engine', 'pug');
 
+// Get Metadata page
 app.get('/nginx', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.render('index', {domain: domain});
 });
 
-app.get('/linux/amd64/DOMAIN/aci/nginx-latest.aci', function (req, res) {
-    res.sendfile(__dirname + '/nginx.aci');
+// Access denied page
+function accessDenied(res) {
+    res.statusCode = 401;
+    res.setHeader('WWW-Authenticate', 'Basic realm=blub');
+    res.send('nope.');
+}
+
+// Upload (experimental)
+app.post('/upload', upload.single('aci'), function (req, res) {
+    res.end();
+    file = req.file.path;
+    dest = 'tar/' + Date.now();
+
+    cmd = 'mkdir ' + dest + ' && tar -xf ' + file + ' -C ' + dest;
+
+    exec(cmd, function (error, stdout, stdderr) {
+        if (error === null) {
+            fs.readFile(dest + '/manifest', 'utf8', function (err, res) {
+                if (!err) {
+                    manifest = JSON.parse(res);
+                    //console.log(manifest);
+
+                    console.log('New ACI:');
+                    console.log(' * Name:     ' + manifest.name);
+                    console.log(' * Version:  ' + (manifest.labels.version || "none"));
+
+                    // Delete extracted tar
+                    exec('rm -r ' + dest);
+                }
+            });
+        }
+    });
+
 });
 
+// Get nginx aci container
+app.get('/linux/amd64/' + domain + '/aci/nginx-latest.aci', function (req, res) {
+
+    if (auth.authorized(req)) {
+        res.sendFile(__dirname + '/nginx.aci');
+    } else {
+        accessDenied(res);
+    }
+});
+
+// Start the server
 app.listen(3002, function () {
-    console.log('Example app listening on port 3002!');
+    console.log('ACI-Server listening on port 3002!');
 });
